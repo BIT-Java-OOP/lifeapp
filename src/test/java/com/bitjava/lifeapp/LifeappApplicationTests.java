3aab00433162e817a20f7b80c64e9fce56aaf191
Ajris <hubiflower@gmail.com>
2019-05-06 18:05:12 +0200

    [master] Initial commit

---
+++ src/test/java/com/bitjava/lifeapp/LifeappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bitjava.lifeapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LifeappApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
